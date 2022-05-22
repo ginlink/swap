261addbcd99466b69bdc2f76ff7a1037b109da5e
ginlink <ginlink1998@gmail.com>
2022-05-22 22:17:30 +0800

    refactor: downgrading storybook

--- packages/uilib/.storybook/main.js
+++ packages/uilib/.storybook/main.js
@@ -6,7 +6,6 @@ module.exports = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
     "themeprovider-storybook/register"
   ],
   framework: "@storybook/react",
