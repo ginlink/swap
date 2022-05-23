bdcb988481c66cd963c91435b8bbba717b019c7f
ginlink <ginlink1998@gmail.com>
2022-05-24 00:28:15 +0800

    feat: add coolswap

--- apps/next12_react17/src/styled.d.ts
+++
@@ -1,7 +0,0 @@
-import 'styled-components'
-import { MyTheme } from '@ginlink/uilib'
-
-declare module 'styled-components' {
-  /* eslint-disable @typescript-eslint/no-empty-interface */
-  export interface DefaultTheme extends MyTheme {}
-}
